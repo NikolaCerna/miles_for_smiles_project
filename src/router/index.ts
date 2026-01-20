10512cb5af34b978b55d002863f4fb27f3003d8e
NikolaCerna <nikolacerna02@gmail.com>
2026-01-20 16:11:51 +0100

    fixed router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: RouteRecordRaw[] = [
@@ -56,7 +56,7 @@ const routes: RouteRecordRaw[] = [
   }
 ]
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
